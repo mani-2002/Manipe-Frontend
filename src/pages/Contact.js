097c1c39fd6b425af3f6db46bd6ccf6cf024d6b6
mani2002 <manikantavinjamuri8522@gmail.com>
2024-02-18 17:19:52 +0530

    revoked

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Contact Page soon
+    </div>
+  );
+}
+
+export default Contact;
