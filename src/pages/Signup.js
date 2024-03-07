8a750f2de54d9a5bb1732f712f373dda0f4b7b28
mani2002 <manikantavinjamuri8522@gmail.com>
2024-03-07 14:22:45 +0530

    added messaging

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -27,13 +27,16 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://manipe-b4st.onrender.com/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://manipe-b4st.onrender.com/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
